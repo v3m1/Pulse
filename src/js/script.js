020599f17f53848a15fe7f2bb18d3709bc518329
Michail <v3m1ka@gmail.com>
2024-02-13 20:58:24 +0700

    add section catalog part 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png" alt=""></button>',
         responsive: [
