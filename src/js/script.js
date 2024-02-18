0a5cc913785d68913f5785a0d89b540e6bcf9eb6
Michail <v3m1ka@gmail.com>
2024-02-18 20:08:22 +0700

    problem

--- src/js/script.js
+++ src/js/script.js
@@ -114,4 +114,22 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(funstion() {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
