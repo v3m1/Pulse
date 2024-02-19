43a047a11d6c5cfed7a95ac3b4929e2e366873db
Michail <v3m1ka@gmail.com>
2024-02-19 20:01:27 +0700

    add aroow page up + css animations

--- src/js/script.js
+++ src/js/script.js
@@ -131,4 +131,19 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+    
+    new WOW().init();
 });
